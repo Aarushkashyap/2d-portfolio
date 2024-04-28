cf6d456a6f74c0b22d6a68a9d509c91e0087e168
Aarush Kashyap <aarush.kashyap21@gmail.com>
2024-04-28 20:17:45 +0530

    Added constant values and dialogues for object collision

---
+++ src/constants.js
@@ -0,0 +1,33 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+      I've made a couple of projects using vanilla javascript. I also like C++ and python. Anyway regardless of the language, I just like programming to make cool stuff.
+      Here is my <a href="https://github.com/Aarushkashyap" target="_blank">Github</a>!`,
+
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+
+  "sofa-table": `That's my sofa. I like to sit here and work on projects. 
+    I also play video games of various genre such as souls-like, multiplayer shooter.`,
+
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+     <a href="https://www.youtube.com/@codebyheart" target="_blank">codebyheart</a>, <a href="https://www.youtube.com/@AniaKubow" target="_blank">AniaKubow</a>,
+    <a href="https://www.youtube.com/@Fireship" target="_blank">Fireship</a> (sometimes) and <a href="https://www.youtube.com/@WilliamFiset-videos" target="_blank">WilliamFiset</a>!`,
+
+  bed: `This is where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it. And sometimes I like to write some poetry here too!`,
+
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/Aarushkashyap/Resume/blob/main/Aarush-SoftwareDeveloper.pdf" target="_blank">Check it out?</a>
+    Contact me at aarush.kashyap21@gmail.com if you have any interesting job opportunities!`,
+
+  projects: `There are some fun projects I have created along the line either to aid in development or just to learn something new, such as:
+  <a href="https://github.com/Aarushkashyap/concepts/tree/dev/drumKit" target="_blank">A Keyboard based Drum kit</a>
+  <a href="https://github.com/Aarushkashyap/concepts/tree/dev/passwordGenerator" target="_blank"> A Random Password Generator</a>
+  <a href="https://github.com/Aarushkashyap/CSVtoJSON" target="_blank"> A CSV to JSON converter </a>`,
+
+  library: `There are a lot of books on my shelves. Who else compulsively buys technical books without ever finishing them?`,
+
+  exit: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by visiting the official docs <a href="https://kaboomjs.com/" target="_blank">here</a>.
+  If you want to exit Aarush's portfolio, just close the tab.`,
+};
